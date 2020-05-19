9d3ca87fce78cd9c4c8fc3977a546fe96234e093
Abdelwahed <abdelwahedkm@gmail.com>
2020-05-19 05:46:45 +0100

    db Image

---
+++ DevopsDB/dbo/Tables/Movies.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Movies] (
+    [MovieId] INT            IDENTITY (1, 1) NOT NULL,
+    [Title]   NVARCHAR (MAX) NULL,
+    [GenreId] INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Movies] PRIMARY KEY CLUSTERED ([MovieId] ASC),
+    CONSTRAINT [FK_dbo.Movies_dbo.Genres_GenreId] FOREIGN KEY ([GenreId]) REFERENCES [dbo].[Genres] ([GenreId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_GenreId]
+    ON [dbo].[Movies]([GenreId] ASC);
+
